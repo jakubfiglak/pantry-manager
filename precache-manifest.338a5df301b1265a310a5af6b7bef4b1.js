d6173b88e99e3495cdbd10ba95496b987f34b8f5
Jakub Figlak <jakub.figlak@gmail.com>
2020-04-05 20:05:03 +0200

    Updates

---
+++ precache-manifest.338a5df301b1265a310a5af6b7bef4b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc8e5e035301f592b6d5e683dede41d5",
+    "url": "//jakubfiglak.github.io/pantry-manager/index.html"
+  },
+  {
+    "revision": "1abc42b3dfb88a540b3c",
+    "url": "//jakubfiglak.github.io/pantry-manager/static/js/2.acbae0c0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "//jakubfiglak.github.io/pantry-manager/static/js/2.acbae0c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "793e68f083fd1b06b5ad",
+    "url": "//jakubfiglak.github.io/pantry-manager/static/js/main.db542609.chunk.js"
+  },
+  {
+    "revision": "f7a3756bbe2619030e1d",
+    "url": "//jakubfiglak.github.io/pantry-manager/static/js/runtime-main.61c8675f.js"
+  }
+]);
